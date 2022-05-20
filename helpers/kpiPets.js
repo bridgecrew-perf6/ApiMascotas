e0c812d609644bf19b21d1c02a6a5d390266053a
victordnb <victordnb@gmail.com>
2022-05-20 11:35:01 +0200

    Added Average + Desviation endpoints

--- helpers/kpiPets.js
+++ helpers/kpiPets.js
@@ -32,3 +32,71 @@ const { ConditionalNodeDependencies } = require('mathjs');
     }
     return dataResult;
 }
+
+exports.averageAge = async function(specie) {
+    let averageAge = 0;
+    let count = 0;
+    let result = 0;
+
+
+    //mongodb query group by species get max
+    await Pet.aggregate([
+        {
+            $group: {
+                _id: "$species",
+                count: { $sum: 1 },
+                averageAge: { $avg: "$age" }
+            }
+        }
+    ]).then(function(data) {
+       //save in objSpecies max count of species
+       console.log(data);
+         data.forEach(function(item) {
+            if(item._id == specie){
+                averageAge = item.averageAge;
+                count = item.count;
+            }
+        });
+        result = averageAge;
+    });
+    //build object 
+    const dataResult = {
+        specie: specie,
+        averageAge: result,
+        Count: count
+    }
+    return dataResult;
+
+}
+
+exports.standardAgeDeviation = async function(specie) {
+    let count = 0;
+    let result = 0;
+
+    await Pet.aggregate([
+        {
+            $group: {
+                _id: "$species",
+                count: { $sum: 1 },
+                stdDevSamp: { $stdDevSamp: "$age" }
+            }
+        }
+    ]).then(function(data) {
+        console.log(data);
+        
+        //desviation:
+        data.forEach(function(item) {
+            if(item._id == specie){
+                count = item.count;
+                result = item.stdDevSamp;
+            }
+        });
+     });
+    //build object
+    const dataResult = {
+        specie: specie,
+        standardAgeDeviation: result,
+        Count: count
+    }
+    return dataResult;
+}
